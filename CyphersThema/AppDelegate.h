a1828f5b3be0720744d5a829b7a4ca96253012c3
krackWingSam <sangwoo21@askstory.com>
2018-01-13 14:55:20 +0900

    first commit

---
+++ CyphersThema/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CyphersThema
+//
+//  Created by askstory on 2018. 1. 13..
+//  Copyright © 2018년 Fermata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CyphersThema/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CyphersThema
+//
+//  Created by askstory on 2018. 1. 13..
+//  Copyright © 2018년 Fermata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
